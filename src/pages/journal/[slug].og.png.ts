68727235aacc852cabad24d29cc668c46476303d
Sam King <mail@samking.co>
2025-08-03 02:01:45 -0600

    Update opengraph images to new font

--- src/pages/journal/[slug].og.png.ts
+++ src/pages/journal/[slug].og.png.ts
@@ -8,78 +8,80 @@ import { formatDate } from "../../utils/dates";
 
 export const prerender = true;
 
-const bodyFont = readFileSync(`${process.cwd()}/public/fonts/Text.woff`);
-const monoFont = readFileSync(`${process.cwd()}/public/fonts/Mono.woff`);
+const commitMonoFont = readFileSync(`${process.cwd()}/public/fonts/CommitMono-400-Regular.ttf`);
+const commitMonoBold = readFileSync(`${process.cwd()}/public/fonts/CommitMono-700-Regular.ttf`);
 
 export async function getStaticPaths() {
-	const entries = await getCollection("posts");
-	return entries.map((entry) => ({
-		params: { slug: entry.slug },
-		props: { entry },
-	}));
+  const entries = await getCollection("posts");
+  return entries.map((entry) => ({
+    params: { slug: entry.slug },
+    props: { entry },
+  }));
 }
 
 export const GET: APIRoute = async ({ props }) => {
-	const post = props.entry;
+  const post = props.entry;
 
-	const title = post.data.title;
-	const excerpt = post.data.excerpt;
-	const date = post.data.date;
+  const title = post.data.title;
+  const excerpt = post.data.excerpt;
+  const date = post.data.date;
 
-	const markup = html(`<div
+  const markup = html(`<div
     style="height: 100%; width: 100%; padding: 80px; display: flex; flex-direction: column; justify-content: space-between; background-color: rgb(0,0,0);"
   >
     <div style="display: flex; flex-direction: column;">
       <div
-        style="font-size: 32px; font-family: Mono; line-height: 56px; margin-top: 16px; display: flex; flex-direction: column; color: white; opacity: 0.5;"
+        style="font-size: 40px; line-height: 60px; font-family: CommitMono; display: flex; flex-direction: column; color: white; opacity: 0.5;"
       >
         ${formatDate(date)}
       </div>
       <div
-        style="font-size: 64px; font-family: Text; display: flex; flex-direction: column; color: white;"
+        style="font-size: 40px; line-height: 60px; font-family: CommitMono; font-weight: 700; display: flex; flex-direction: column; color: white;"
       >
         ${title}
       </div>
       <div
-        style="font-size: 40px; font-family: Text; line-height: 56px; margin-top: 16px; display: flex; flex-direction: column; color: white; opacity: 0.5;"
+        style="font-size: 40px; line-height: 60px; font-family: CommitMono; display: flex; flex-direction: column; color: white; opacity: 0.5;"
       >
         ${excerpt.slice(0, 160)}${excerpt.length > 160 ? "…" : ""}
       </div>
     </div>
 
     <div
-      style="font-size: 32px; font-family: Text; line-height: 56px; margin-top: 16px; display: flex; flex-direction: column; color: white; opacity: 0.5;"
+      style="font-size: 40px; line-height: 60px; font-family: CommitMono; display: flex; flex-direction: column; color: white; opacity: 0.5;"
     >
       samking.co
     </div>
   </div>`);
 
-	const svg = await satori(markup, {
-		width: 1200,
-		height: 630,
-		fonts: [
-			{
-				name: "Text",
-				data: bodyFont,
-				style: "normal",
-			},
-			{
-				name: "Mono",
-				data: monoFont,
-				style: "normal",
-			},
-		],
-	});
+  const svg = await satori(markup, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        name: "CommitMono",
+        data: commitMonoFont,
+        style: "normal",
+        weight: 400,
+      },
+      {
+        name: "CommitMono",
+        data: commitMonoBold,
+        style: "normal",
+        weight: 700,
+      },
+    ],
+  });
 
-	const png = await sharp(Buffer.from(svg)).png().toBuffer();
+  const png = await sharp(Buffer.from(svg)).png().toBuffer();
 
-	return new Response(png, {
-		status: 200,
-		headers: {
-			"Content-Type": "image/png",
-			"Content-Length": png.byteLength.toString(),
-			"Cache-Control": "s-maxage=1, stale-while-revalidate=59",
-			"Content-Disposition": `inline; filename=og_${post.slug}.png`,
-		},
-	});
+  return new Response(png, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/png",
+      "Content-Length": png.byteLength.toString(),
+      "Cache-Control": "s-maxage=1, stale-while-revalidate=59",
+      "Content-Disposition": `inline; filename=og_${post.slug}.png`,
+    },
+  });
 };
--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -8,69 +8,64 @@ import sharp from "sharp";
 export const prerender = true;
 
 const avatarBase64 = readFileSync("public/avatar.png", "base64");
-const displayFont = readFileSync("public/fonts/Nikolai-Italic.woff");
-const bodyFont = readFileSync("public/fonts/Text.woff");
-const monoFont = readFileSync("public/fonts/Mono.woff");
+const commitMonoFont = readFileSync("public/fonts/CommitMono-400-Regular.ttf");
+const commitMonoBold = readFileSync("public/fonts/CommitMono-700-Regular.ttf");
 
 export const GET: APIRoute = async () => {
-	const title = "Sam King";
-	const excerpt = "Photography, Software, and Design\nVancouver, Canada";
+  const title = "Sam King";
+  const excerpt = "Photography, Software, and Design\nVancouver, Canada";
 
-	const markup = html(`<div
-    style="height: 100%; width: 100%; padding: 80px; display: flex; flex-direction: row; align-items: flex-end; justify-content: space-between; background-color: rgb(0,0,0);"
+  const markup = html(`<div
+    style="height: 100%; width: 100%; padding: 80px; display: flex; flex-direction: column; gap: 60px; background-color: rgb(0,0,0); justify-content: flex-end;"
   >
+    <img src="data:image/png;base64,${avatarBase64}" width="240" height="240" />
     <div style="display: flex; flex-direction: column;">
-      <div style="display: flex; flex-direction: column; padding-bottom: 40px;">
+      <div style="display: flex; flex-direction: column;">
         <div
-          style="font-size: 96px; line-height: 1; font-family: Nikolai; display: flex; flex-direction: column; color: white; margin-bottom: 16px;"
+          style="font-size: 40px; line-height: 40px; font-family: CommitMono; font-weight: 700; color: white; margin-bottom: 20px;"
         >
           ${title}
         </div>
 
         ${excerpt.split("\n").map(
-					(line) => `<div
-          style="font-size: 40px; font-family: Text; line-height: 1; display: flex; flex-direction: column; color: white; opacity: 0.5;"
+          (line) => `<div
+          style="font-size: 40px; line-height: 40px; font-family: CommitMono; color: white; opacity: 0.5;"
         >
           ${line}
         </div>`,
-				)}
+        )}
       </div>
     </div>
-
-    <img src="data:image/png;base64,${avatarBase64}" style="margin-left: 80px;" width="380" height="380" />
   </div>`);
 
-	const svg = await satori(markup, {
-		width: 1200,
-		height: 630,
-		fonts: [
-			{
-				name: "Nikolai",
-				data: displayFont,
-				style: "italic",
-			},
-			{
-				name: "Text",
-				data: bodyFont,
-				style: "normal",
-			},
-			{
-				name: "Mono",
-				data: monoFont,
-				style: "normal",
-			},
-		],
-	});
+  const svg = await satori(markup, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        name: "CommitMono",
+        data: commitMonoFont,
+        style: "normal",
+        weight: 400,
+      },
+      {
+        name: "CommitMono",
+        data: commitMonoBold,
+        style: "normal",
+        weight: 700,
+      },
+    ],
+  });
 
-	const png = await sharp(Buffer.from(svg)).png().toBuffer();
+  const png = await sharp(Buffer.from(svg)).png().toBuffer();
 
-	return new Response(png, {
-		status: 200,
-		headers: {
-			"Content-Type": "image/png",
-			"Content-Length": png.byteLength.toString(),
-			"Cache-Control": "s-maxage=1, stale-while-revalidate=59",
-			"Content-Disposition": "inline; filename=og.png",
-		},
-	});
+  return new Response(png, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/png",
+      "Content-Length": png.byteLength.toString(),
+      "Cache-Control": "s-maxage=1, stale-while-revalidate=59",
+      "Content-Disposition": "inline; filename=og.png",
+    },
+  });
 };
