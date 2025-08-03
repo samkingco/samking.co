c878dd1c1ad810dd3c729b97efa26974dec162ae
Sam King <mail@samking.co>
2025-08-03 11:13:44 -0600

    Update deps and new tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import { FontaineTransform } from "fontaine";
 import { rehypeImageCaptions } from "./src/utils/rehype-image-captions.mjs";
 
 export default defineConfig({
 	site: "https://samking.co",
 	output: "static",
-	integrations: [tailwind(), sitemap()],
+	integrations: [sitemap()],
 	markdown: {
 		rehypePlugins: [rehypeImageCaptions],
 	},
 	vite: {
 		plugins: [
+			tailwindcss(),
 			FontaineTransform.vite({
 				fallbacks: ["Arial"],
 				resolvePath: (id) => new URL(`./public${id}`, import.meta.url),
